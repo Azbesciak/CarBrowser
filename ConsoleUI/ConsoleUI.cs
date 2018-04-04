f37e8aca91b4ea67fe115afdbf0296795f103ce4
Witold Kupś <witold.kups@interlan.pl>
2018-04-04 20:21:21 +0200

    Renamed constant fields to pascal case.

--- ConsoleUI/ConsoleUI.cs
+++ ConsoleUI/ConsoleUI.cs
@@ -7,10 +7,10 @@ namespace Kups.CarBrowser.ConsoleUI
 {
     public static class ConsoleUi
     {
-        private const string LIST_CARS_COMMAND = "cars";
-        private const string LIST_DEALERS_COMMAND = "dealers";
-        private const string SHOW_HELP_COMMAND = "help";
-        private const string EXIT_COMMAND = "exit";
+        private const string ListCarsCommand = "cars";
+        private const string ListDealersCommand = "dealers";
+        private const string ShowHelpCommand = "help";
+        private const string ExitCommand = "exit";
 
         public static void Main(string[] args)
         {
@@ -21,18 +21,18 @@ namespace Kups.CarBrowser.ConsoleUI
                 var command = Console.ReadLine();
                 if (command == null)
                     continue;
-                if (command.StartsWith(EXIT_COMMAND)) return;
+                if (command.StartsWith(ExitCommand)) return;
 
                 var commandSplit = Regex.Split(command, "\\s");
                 switch (commandSplit[0])
                 {
-                    case LIST_CARS_COMMAND:
+                    case ListCarsCommand:
                         PrintResult(carBrowser.CarsService, commandSplit);
                         break;
-                    case LIST_DEALERS_COMMAND:
+                    case ListDealersCommand:
                         PrintResult(carBrowser.DealersService, commandSplit);
                         break;
-                    case SHOW_HELP_COMMAND:
+                    case ShowHelpCommand:
                         PrintInstructions();
                         break;
                     default:
@@ -58,10 +58,10 @@ namespace Kups.CarBrowser.ConsoleUI
         private static void PrintInstructions()
         {
             Console.WriteLine("Options:\n" +
-                              $"{LIST_CARS_COMMAND} [id]: List a car if id specified, otherwise - all\n" +
-                              $"{LIST_DEALERS_COMMAND} [id]: List a dealer if id specified, otherwise - all\n" +
-                              $"{SHOW_HELP_COMMAND}: shows instructions list\n" +
-                              $"{EXIT_COMMAND}: shut down program");
+                              $"{ListCarsCommand} [id]: List a car if id specified, otherwise - all\n" +
+                              $"{ListDealersCommand} [id]: List a dealer if id specified, otherwise - all\n" +
+                              $"{ShowHelpCommand}: shows instructions list\n" +
+                              $"{ExitCommand}: shut down program");
         }
     }
 }
