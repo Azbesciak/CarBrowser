fba68a91d4181652eb0201bd900b4c3d3e7771c0
Witold Kupś <witold.kups@interlan.pl>
2018-05-02 09:19:18 +0200

    Code cleaning

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Kups.CarBrowser.BL;
 using WPFUI.Models;
 
@@ -20,7 +10,7 @@ namespace WPFUI
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         private static readonly CarBrowser CarBrowser = new CarBrowser();
         private readonly List<Lazy<object>> _pages = new List<Lazy<object>>(new[]
--- WPFUI/Views/DealersView.xaml.cs
+++ WPFUI/Views/DealersView.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using Kups.CarBrowser.BO;
-using Kups.CarBrowser.Core;
-
-namespace WPFUI.Views
+﻿namespace WPFUI.Views
 {
     /// <summary>
     /// Logika interakcji dla klasy DealersView.xaml
     /// </summary>
-    public partial class DealersView : UserControl
+    public partial class DealersView
     {
        
         public DealersView()
