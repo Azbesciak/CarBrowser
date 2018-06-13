6ef20764dd511001da924aa2bf197646de54ed46
Witold Kupś <witold.kups@interlan.pl>
2018-06-13 10:53:22 +0200

    Custom converter from string to int (default 0 if empty), visually disable readonly fields

---
+++ WPFUI/StringToIntConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFUI
+{
+    [ValueConversion(typeof(int), typeof(string))]
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) => value?.ToString() ?? "";
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            int.TryParse(value?.ToString(), out var res);
+            return res;
+        }
+    }
+}
